9738f599880a2f4b8d3303bf256784bdc1ca36c6
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-03-27 12:06:40 +0300

    add initial page

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+function fizzBuzz(number){
+
+}
+function fizzBuzz(number){
+
+return number;
+
+}
+function fizzBuzz(number){
+
+
+//here in the codeblock we want to 
+
+      //deal with each number represented by the variable (number)   
+
+     console.log(i);
+}
+function fizzBuzz(number){
+
+
+if (number%3 === 0) console.log("Fizz");
+
+      else if(number%5 === 0) console.log("Buzz");
+
+      else if(number%3 === 0 && number %5 ===0) console.log("FizzBuzz");
+
+      else console.log(number);
+}
+function fizzBuzz(number){
+
+if    (number%3 === 0 && number %5 ===0) console.log("FizzBuzz");
+
+      else if (number%3 === 0) console.log("Fizz");
+
+      else if (number%5 === 0) console.log("Buzz");     
+
+      else console.log(number);
+}
+fizzBuzz(15)
+function fizzBuzz(number){
+
+if    (number%6 === 0 && number%5 ===0 && number%3 === 0) console.log("bing");
+
+      else if (number%3 === 0) console.log("Fizz");
+
+      else if (number%15 === 0) console.log("FizzBuzz")
+
+      else if (number%5 === 0) console.log("Buzz");
+
+      else console.log(number);
+}
