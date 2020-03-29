f30c3930f77ecbf34774e86b1320a463f693c9ea
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-03-29 18:11:10 +0300

    change result color

--- js/scripts.js
+++ js/scripts.js
@@ -16,19 +16,25 @@ document.getElementById("submit").addEventListener("click",function(event){
     
       if (dd === "---"){
       document.getElementById("Invalid").innerText = "Please enter a valid date!"
+      document.getElementById("Invalid").style.color ="blue"
       } else if(mm === "---"){
       document.getElementById("Invalid1").innerText = "Please select a month!"
+      document.getElementById("Invalid1").style.color ="blue"
       } else if(yy <= 1899 || yy> 2020){
       document.getElementById("Invalid2").innerText = "Please enter a valid year!"
+      document.getElementById("Invalid2").style.color ="blue"
       }else if (gender === "Male"){
       document.getElementById("Result").innerHTML = "🎉️🎉️You were born on a " + days[dayOfTheWeek] + ".\n" +  "Your Akan name is " + maleNames[dayOfTheWeek] + "🎉️🎉️"
       document.getElementById("Result").style.fontSize = "50px"
       document.getElementById("Result").style.textAlign = "center"
+      document.getElementById("Result").style.color ="Brown"
       }else if (gender === "Female"){
       document.getElementById("Result").innerHTML = "🎉️🎉️You were born on a " + days[dayOfTheWeek] + ".\n" + "Your Akan name is " + femaleNames[dayOfTheWeek] + "🎉️🎉️"
       document.getElementById("Result").style.fontSize = "50px"
       document.getElementById("Result").style.textAlign = "center"
+      document.getElementById("Result").style.color ="Purple"
       }else {
       document.getElementById("Invalid3").innerText = "Please select Gender!"
+      document.getElementById("Invalid3").style.color ="blue"
       }
     }
